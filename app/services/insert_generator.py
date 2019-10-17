3a48d02f001cb35f222be9939d51d81f93950e6a
htrinter <htdev@posteo.de>
2019-10-17 12:53:21 +0200

    Do some cleanup

--- app/services/insert_generator.py
+++ app/services/insert_generator.py
@@ -5,17 +5,17 @@ from shutil import copyfile, rmtree
 
 from services.insert_cache_manager import copy_to_cache, get_cache_file_name
 
-TMP_DIR_PATH = "generated-files/tmp";
+TMP_DIR_PATH = "generated-files/tmp"
 
 def generate_file(format, layout, language, year):
     generation_directory = init_generation_directory()
 
-    alfieArg = "{}-{}-{}-{}-no-no-yes".format(format, layout, language, year)
-    alfiePdfFileName = "diary-{}-{}-{}.pdf".format(format, year, language)
-    subprocess.run(["python3", "alfie.py", alfieArg], cwd=generation_directory, timeout=10)
+    alfie_arg = "{}-{}-{}-{}-no-no-yes".format(format, layout, language, year)
+    alfie_pdf_file_name = "diary-{}-{}-{}.pdf".format(format, year, language)
+    subprocess.run(["python3", "alfie.py", alfie_arg], cwd=generation_directory, timeout=10)
 
     cache_file_name = get_cache_file_name(format, layout, year, language)
-    cache_file_path = copy_to_cache(generation_directory + "/" + alfiePdfFileName, cache_file_name)
+    cache_file_path = copy_to_cache(generation_directory + "/" + alfie_pdf_file_name, cache_file_name)
 
     delete_generation_directory(generation_directory)
 
